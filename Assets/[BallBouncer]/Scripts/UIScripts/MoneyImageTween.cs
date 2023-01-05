ef1dddf573d19783e0fbd945607f21517eaa79fd
Deniz Kaymakci <deniz.kaymakci@hotmail.com>
2023-01-05 15:25:14 +0300

    Updae Money ImageTween.cs

--- Assets/[BallBouncer]/Scripts/UIScripts/MoneyImageTween.cs
+++ Assets/[BallBouncer]/Scripts/UIScripts/MoneyImageTween.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class MoneyImageTween : MonoBehaviour
